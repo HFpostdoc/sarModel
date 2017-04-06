e34b37d90ab70091d8f2f2a768d7ae490bd3654b
mklau <matthewklau@fas.harvard.edu>
2017-04-06 17:19:50 -0400

    rms sarModel

--- R/sarModel.R
+++
@@ -1,9 +0,0 @@
-#' Simulated the dynamics of the inquiline food web inside Sarracenia pitcher plants 
-#'
-#' The sarModel package provides a set of tools for modeling food web dynamics
-#' based on the Sarracenia purpurea micro-ecosystem.
-#'
-#' The main workhorse function in \pkg{sarModel} is \code{\link{ppSim}}. 
-#'
-"_PACKAGE"
-#> [1] "_PACKAGE"
